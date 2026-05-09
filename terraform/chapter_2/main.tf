47300d552a8a92db0336d9d558095f895137c051
João Squinelato <joao.squinelato97@gmail.com>
2026-05-09 09:57:58 -0300

    initial commit

---
+++ terraform/chapter_2/main.tf
@@ -0,0 +1,45 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_ami" "ubuntu" {
+
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+
+}
+
+output "aws_ami" {
+  value = data.aws_ami.ubuntu
+}
+
+resource "aws_instance" "example" {
+
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "just a example tag"
+  }
+
+}
